d1b34db0bec94276636fccee0a19c4c370b7f0fc
dartora <rodolfo-dartora>
2022-09-02 09:51:27 -0300

    Lesson81

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,63 +1,38 @@
-/* eslint-disable no-console */
 /* eslint-disable no-undef */
-/* eslint-disable func-names */
-/* eslint-disable prefer-arrow-callback */
-describe('Main', function () {
-  // describe.skip('Method A', function () {
-  //   context('Case 1 ', function () {
-  //     it('should happen blabla', () => {
-  //       // espero que aconteca
-  //     });
-  //   });
-  //   context('Case 2', function () {
-  //     it('should happen blabla', () => {
-  //       // espero que aconteca
-  //       throw new Error('just an error');
-  //     });
-  //     it('should happen mimimi', () => {
-  //       // espero que aconteca
-  //       throw new Error('just an error');
-  //     });
-  //   });
-  // });
+const { expect } = require('chai');
 
-  describe('Method B', function () {
-    // hooks
-    let arr = [];
-    before(function () {
-      // roda uma vez, antes do bloco
-      // inicia conexao com banco
-      // cria base de dados
-    });
+describe('Main', () => {
+  // hooks
+  let arr = [];
 
-    after(function () {
-      // roda uma vez, dps do bloco
-      // fecha conexao com o banco
-      // apaga base de dados
-    });
+  beforeEach(() => {
+    // roda tds vezes, antes do bloco
+    arr = [1, 2, 3];
+  });
 
-    beforeEach(function () {
-      // roda tds vezes, antes do bloco
-      arr = [1, 2, 3];
-    });
+  afterEach(() => {
+    // roda rds vezes, depois do bloco
+  });
 
-    afterEach(function () {
-      // roda rds vezes, depois do bloco
-    });
+  // testar tipos ou se já foram criados (smoke test)
+  it('should be an array', () => {
+    expect(arr).to.be.an('array');
+  });
 
-    it('should have a size of 4 when push another value to the array', function () {
-      // const arr = [1, 2, 3];
-      arr.push(4); // 4
-    });
+  it('should have a size of 4 when push another value to the array', () => {
+    // const arr = [1, 2, 3];
+    arr.push(4); // 4
+    expect(arr).to.have.lengthOf(4);
+  });
 
-    it('should have a size of 2 when pop a value to the array', () => {
-      // const arr = [1, 2, 3];
-      arr.pop(3); // 2
-    });
+  it('should remove the value 3 when use pop in the array', () => {
+    arr.pop(); // true
+    expect(arr).to.not.include(3);
+  });
 
-    it('should remove the value 3 when use pop in the array', () => {
-      // const arr = [1, 2, 3];
-      console.log(arr.pop() === 3); // true
-    });
+  it('should have a size of 2 when pop a value to the array', () => {
+    // const arr = [1, 2, 3];
+    arr.pop(3); // 2
+    expect(arr).to.have.length(2);
   });
 });
